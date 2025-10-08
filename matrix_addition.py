7b1bc955db4cc17ced432fe13e1f3e59c9677c33
rahulpr1999 <rahulpr9780@gmail.com>
2025-10-08 08:33:35 +0530

    Create matrix_addition.py

---
+++ matrix_addition.py
@@ -0,0 +1,7 @@
+import numpy as np
+matrix1 = np.array([[1,2,3],[4,5,6],[7,8,9]])
+matrix2 = np.array([[9,8,7],[6,5,4],[3,2,1]])
+print("Matrix 1\n",matrix1)
+print("Matrix 2\n",matrix2)
+result_matrix=np.add(matrix1,matrix2)
+print("result: \n",result_matrix)
